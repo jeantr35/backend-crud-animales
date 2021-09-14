01517958fa393996a58dd5d6f39aaa2fd7b8e217
mvm11 <mvalenciaminota@gmail.com>
2021-09-14 15:43:50 -0500

    Se inicializa el proyecto en SpringBoot

---
+++ src/main/java/com/example/CrudAnimales/CrudAnimalesApplication.java
@@ -0,0 +1,13 @@
+package com.example.CrudAnimales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAnimalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAnimalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CrudAnimales/CrudAnimalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CrudAnimales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAnimalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
