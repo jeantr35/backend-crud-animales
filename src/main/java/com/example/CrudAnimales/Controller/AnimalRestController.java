c1451e921b14088236d1cc85e15b0fd5689a4dd8
jeantr35 <jean.trujillo.35@gmail.com>
2021-09-14 16:27:18 -0500

    Anadidos los metodos de listar y listar por id en AnimalController

--- src/main/java/com/example/CrudAnimales/Controller/AnimalRestController.java
+++ src/main/java/com/example/CrudAnimales/Controller/AnimalRestController.java
@@ -1,7 +1,12 @@
 package com.example.CrudAnimales.Controller;
 
+import com.example.CrudAnimales.Entity.Animal;
+import com.example.CrudAnimales.Services.AnimalServices;
+
 import com.example.CrudAnimales.Services.InterfaceAnimalServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +17,15 @@ public class AnimalRestController {
     @Autowired
     private InterfaceAnimalServices service;
 
+    @GetMapping(value = "/getAnimals")
+    public  Iterable<Animal> getAnimals(){
+        return  service.List();
+    }
+
+    @GetMapping(value = "/getAnimal/{id}")
+    public Animal getAnimalById(@PathVariable int id){
+        return service.ListById(id);
+    }
 
 
 }
