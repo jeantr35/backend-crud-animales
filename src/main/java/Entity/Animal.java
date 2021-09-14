6ef8500176c651ae7110ca72e081f963aad5d97b
mvm11 <mvalenciaminota@gmail.com>
2021-09-14 15:52:25 -0500

    Se añade la clase Animal con sus respectivos atributos y metodos

---
+++ src/main/java/Entity/Animal.java
@@ -0,0 +1,59 @@
+package Entity;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Animal {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String weight;
+    private String type;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", weight='" + weight + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+}
