891802f8ddb9d72e00c5680505fc0b88fe8e3f97
jeantr35 <jean.trujillo.35@gmail.com>
2021-09-14 16:38:35 -0500

    Anadidos los metodos restantes que completan el crud

--- src/main/java/com/example/CrudAnimales/Controller/AnimalRestController.java
+++ src/main/java/com/example/CrudAnimales/Controller/AnimalRestController.java
@@ -5,10 +5,7 @@ import com.example.CrudAnimales.Services.AnimalServices;
 
 import com.example.CrudAnimales.Services.InterfaceAnimalServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/animal")
@@ -25,6 +22,21 @@ public class AnimalRestController {
     @GetMapping(value = "/getAnimal/{id}")
     public Animal getAnimalById(@PathVariable int id){
         return service.ListById(id);
+    }
+
+    @PostMapping(value = "/saveAnimal")
+    public Animal saveAnimal(@RequestBody Animal animal){
+        return service.save(animal);
+    }
+
+    @DeleteMapping(value = "deleteAnimal/{id}")
+    public void deleteAnimal(@PathVariable int id){
+        service.delete(id);
+    }
+
+    @PutMapping(value = "/updateAnimal/{id}")
+    public Animal updateAnimal(@RequestBody Animal animal, @PathVariable int id){
+        return service.update(animal,id);
     }
 
 
